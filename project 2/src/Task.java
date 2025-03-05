c46d38e54d9fc39dfbd028af34e5264ce4eff445
Redstone69 <89121962+Redstone69@users.noreply.github.com>
2025-03-05 00:52:49 -0500

    Add files via upload

---
+++ project 2/src/Task.java
@@ -0,0 +1,44 @@
+public class Task {
+    private String title;
+    private String description;
+    private int priority;
+
+    public Task(String description, String title, int priority) {
+        this.description = description;
+        this.title = title;
+        this.priority = priority;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", priority=" + priority +
+                '}';
+    }
+}
